ca3824c36aa2a73c57c46d4ed403635837f3be03
henrique <pedrohenriquemenezes76@gmail.com>
2026-02-19 14:01:31 -0300

    Dia 17: testes de validação e regras de negócio do serviço de clima

--- src/test/java/model/WeatherInfoTest.java
+++ src/test/java/model/WeatherInfoTest.java
@@ -1,5 +1,4 @@
 package model;
-
 import br.com.desafiosemana4.model.WeatherInfo;
 import org.junit.jupiter.api.Test;
 
@@ -8,35 +7,45 @@ import static org.junit.jupiter.api.Assertions.*;
 class WeatherInfoTest {
 
     @Test
-    void deveCriarObjetoWeatherInfoCorretamente() {
-
+    void deveCriarWeatherInfoComDadosCorretos() {
         WeatherInfo info = new WeatherInfo(
                 "São Paulo",
                 25.0,
-                26.5,
+                27.0,
                 "céu limpo",
                 60
         );
 
-        assertNotNull(info);
+        assertEquals("São Paulo", info.getCidade());
     }
 
     @Test
-    void deveArmazenarDadosCorretamente() {
-
+    void deveRetornarTemperaturaESensacaoCorretas() {
         WeatherInfo info = new WeatherInfo(
                 "Rio de Janeiro",
-                30.0,
-                32.0,
+                30.5,
+                33.0,
                 "ensolarado",
                 55
         );
 
-        assertEquals("Rio de Janeiro", info.getCidade());
-        assertEquals(30.0, info.getTemperatura());
-        assertEquals(32.0, info.getSensacao());
-        assertEquals("ensolarado", info.getDescricao());
-        assertEquals(55, info.getUmidade());
+        assertEquals(30.5, info.getTemperatura());
+        assertEquals(33.0, info.getSensacao());
+    }
+
+    @Test
+    void deveRetornarDescricaoEUmidadeCorretas() {
+        WeatherInfo info = new WeatherInfo(
+                "Curitiba",
+                18.0,
+                17.0,
+                "nublado",
+                80
+        );
+
+        assertEquals("nublado", info.getDescricao());
+        assertEquals(80, info.getUmidade());
     }
 }
+
 
